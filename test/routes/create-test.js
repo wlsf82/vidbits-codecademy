142b3c8c0a8bf8a215bda97f5000fdf6cf7a9e51
Walmyr Filho <wlsf82@gmail.com>
2018-01-03 13:50:18 +0100

    update routes test with new test case and db setup

--- test/routes/create-test.js
+++ test/routes/create-test.js
@@ -2,11 +2,18 @@ const { assert } = require("chai");
 const request = require("supertest");
 
 const app = require("../../app");
+const Video = require("../../models/video");
+
+const { connectDatabase, disconnectDatabase } = require("../database-utilities");
 
 describe("Server path: /videos", () => {
     describe("POST", () => {
+        beforeEach(connectDatabase);
+
+        afterEach(disconnectDatabase);
+
         it("returns created success status response code", async () => {
-            const newVideo ={
+            const newVideo = {
                 title: "Sample title",
                 description: "Sample description"
             };
@@ -17,6 +24,23 @@ describe("Server path: /videos", () => {
                 .send(newVideo);
 
             assert.equal(response.status, 201);
+        });
+
+        it("stores new video in the database", async () => {
+            const newVideoDb = {
+                title: "Sample title db",
+                description: "Sample description db"
+            };
+
+            const response = await request(app)
+                .post("/videos")
+                .type("form")
+                .send(newVideoDb);
+
+            const createdVideo = await Video.findOne(newVideoDb);
+
+            assert.equal(createdVideo.title, newVideoDb.title);
+            assert.equal(createdVideo.description, newVideoDb.description);
         });
     });
 });
