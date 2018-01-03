b95bb33aab17653c95e226e6edcefdcbe6a8c8c8
Walmyr Filho <wlsf82@gmail.com>
2018-01-03 15:04:31 +0100

    update post route for video creation

--- routes/videos.js
+++ routes/videos.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
 
+const Video = require("../models/video");
+
 router.post("/videos", async (req, res, next) => {
+    const { title, description } = req.body;
+
+    await Video.create({ title, description });
+
     res.status(201).send();
 });
 
