e9e725e340e0b616fab63e58a319ee991772794a
Walmyr Filho <wlsf82@gmail.com>
2018-01-03 14:01:23 +0100

    create model tests

--- test/models/video-test.js
+++ test/models/video-test.js
@@ -2,3 +2,31 @@ const { assert } = require("chai");
 const { mongoose, databaseUrl, options } = require("../../database");
 
 const { connectDatabase, disconnectDatabase } = require("../database-utilities");
+
+const Video = require("../../models/video");
+
+describe("Model: Video", () => {
+    beforeEach(connectDatabase);
+
+    afterEach(disconnectDatabase);
+
+    describe("title", () => {
+        it("is a String", () => {
+            const titleAsInt = 1;
+
+            const video = new Video({ title: titleAsInt });
+
+            assert.strictEqual(video.title, titleAsInt.toString());
+        });
+    });
+
+    describe("description", () => {
+        it("is a String", () => {
+            const descriptionAsInt = 1;
+
+            const video = new Video({ description: descriptionAsInt });
+
+            assert.strictEqual(video.description, descriptionAsInt.toString());
+        });
+    });
+});
