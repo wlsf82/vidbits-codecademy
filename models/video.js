3f572ea94bf4dec2505ba4a51015f20d1f321abb
Walmyr Filho <wlsf82@gmail.com>
2018-01-03 14:01:48 +0100

    update model with title, description and its types

--- models/video.js
+++ models/video.js
@@ -1,10 +1,11 @@
 const {mongoose} = require('../database');
 
 const Video = mongoose.model(
-  'Video',
-  mongoose.Schema({
-    
-  })
+    'Video',
+    mongoose.Schema({
+        title: { type: String },
+        description: { type: String },
+    })
 );
 
 module.exports = Video;
