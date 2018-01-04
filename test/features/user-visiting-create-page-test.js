a024cf1cafed4280a317556aa03f28872e3d0853
Walmyr Filho <wlsf82@gmail.com>
2018-01-04 12:25:32 +0100

    update feature test for video creation and add a new one
    The description of the existing test was wrong and there was no
    test case for checking that the created video was rendered on the
    landing page.

--- test/features/user-visiting-create-page-test.js
+++ test/features/user-visiting-create-page-test.js
@@ -2,18 +2,26 @@ const { assert } = require("chai");
 
 describe("User visits the create video page", () => {
     describe("User fill out a form and submit", () => {
-        it("returns to the landing page with and the submited information is there", () => {
-            const title = "Sample video title";
-            const description = "Sample video description";
+        const title = "Sample video title";
+        const description = "Sample video description";
 
+        beforeEach(() => {
             browser.url("/videos/create");
 
             browser.setValue("#video-title-input", title);
             browser.setValue("#video-description-input", description);
             browser.click("#submit-button");
+        });
 
+        it("renders the created video on its on page", () => {
             assert.include(browser.getText("#videos-container h3"), title);
             assert.include(browser.getText("#videos-container p"), description);
+        });
+
+        it("renders the created video on the landing page", () => {
+            browser.url("/");
+
+            assert.include(browser.getText("#videos-container .video-title"), title);
         });
     });
 });
