090498d7954f556e6e356f53861d662213adc8f9
Walmyr Filho <wlsf82@gmail.com>
2018-01-04 14:42:59 +0100

    update video model to make title a required field

--- models/video.js
+++ models/video.js
@@ -3,7 +3,10 @@ const { mongoose } = require("../database");
 const Video = mongoose.model(
     "Video",
     mongoose.Schema({
-        title: { type: String },
+        title: {
+            type: String,
+            required: true,
+        },
         description: { type: String },
     })
 );
