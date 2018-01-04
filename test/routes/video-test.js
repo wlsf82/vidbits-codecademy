4836a2335d015cd1559c53b44c9acb0b26dd34a8
Walmyr Filho <wlsf82@gmail.com>
2018-01-04 17:40:00 +0100

    add get route test for single video rendering

---
+++ test/routes/video-test.js
@@ -0,0 +1,24 @@
+const { assert } = require("chai");
+const request = require("supertest");
+
+const app = require("../../app");
+
+const { connectDatabase, disconnectDatabase } = require("../database-utilities");
+const { parseTextFromHTML, seedVideoToDatabase  } = require("../test-utils");
+
+describe("Server path: /videos/:id", () => {
+    describe("GET", () => {
+        beforeEach(connectDatabase);
+
+        afterEach(disconnectDatabase);
+
+        it("render single video", async () => {
+            const video = await seedVideoToDatabase({});
+
+            const response = await request(app).get(`/videos/${video._id}`);
+
+            assert.include(parseTextFromHTML(response.text, "#videos-container h3"), video.title);
+            assert.include(parseTextFromHTML(response.text, "#videos-container p"), video.description);
+        });
+    });
+});
