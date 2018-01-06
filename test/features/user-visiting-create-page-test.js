d1cfe665acdb2ad7ba68829148683ee4593719b2
Walmyr Filho <wlsf82@gmail.com>
2018-01-06 19:18:26 +0100

    add feature tests for video creation with missing title or url

--- test/features/user-visiting-create-page-test.js
+++ test/features/user-visiting-create-page-test.js
@@ -20,4 +20,42 @@ describe("User visits the create video page", () => {
             assert.include(browser.getText("#videos-container p"), description);
         });
     });
+
+    describe("User fill out a form with missing title and submit", () => {
+        const description = "Sample video description";
+        const url = "http://example.com";
+
+        beforeEach(() => {
+            browser.url("/videos/create");
+
+            browser.setValue("#video-description-input", description);
+            browser.setValue("#video-url-input", url);
+            browser.click("#submit-button");
+        });
+
+        it("shows a error regarding title being required and keep the other fields", () => {
+            assert.equal(browser.getText("#create-video-container form span"), "Path `title` is required.");
+            assert.equal(browser.getAttribute("#video-description-input", "value"), description);
+            assert.equal(browser.getAttribute("#video-url-input", "value"), url);
+        });
+    });
+
+    describe("User fill out a form with missing url and submit", () => {
+        const title = "Sample video title";
+        const description = "Sample video description";
+
+        beforeEach(() => {
+            browser.url("/videos/create");
+
+            browser.setValue("#video-title-input", title);
+            browser.setValue("#video-description-input", description);
+            browser.click("#submit-button");
+        });
+
+        it("shows a error regarding url being required and keep the other fields", () => {
+            assert.equal(browser.getAttribute("#video-title-input", "value"), title);
+            assert.equal(browser.getAttribute("#video-description-input", "value"), description);
+            assert.equal(browser.getText("#create-video-container form span"), "Path `url` is required.");
+        });
+    });
 });
