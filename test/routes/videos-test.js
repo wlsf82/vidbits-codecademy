b284a32babd9ca06f13595bf30d417a1bdac370f
Walmyr Filho <wlsf82@gmail.com>
2018-01-07 20:27:19 +0100

    add get and post route tests for video deletion

--- test/routes/videos-test.js
+++ test/routes/videos-test.js
@@ -2,6 +2,7 @@ const { assert } = require("chai");
 const request = require("supertest");
 
 const app = require("../../app");
+const Video = require("../../models/video");
 
 const { connectDatabase, disconnectDatabase } = require("../database-utilities");
 const { parseTextFromHTML, seedVideoToDatabase } = require("../test-utils");
@@ -18,6 +19,43 @@ describe("Server path: /videos", () => {
             const response = await request(app).get(`/videos`);
 
             assert.include(parseTextFromHTML(response.text, "#videos-container .video-title"), video.title);
+        });
+    });
+});
+
+describe("Server path: /videos/:id/delete", () => {
+    describe("GET", () => {
+        beforeEach(connectDatabase);
+
+        afterEach(disconnectDatabase);
+
+        it("does not show deleted video on the landing page and returns '302' status code", async () => {
+            const video = await seedVideoToDatabase({});
+
+            const response = await request(app)
+                .post(`/videos/${video._id}/delete`)
+                .send();
+
+            assert.equal(response.status, 302);
+            assert.equal(response.headers.location, "/");
+        });
+    });
+
+    describe("POST", () => {
+        beforeEach(connectDatabase);
+
+        afterEach(disconnectDatabase);
+
+        it("deletes video from the database", async () => {
+            const video = await seedVideoToDatabase({});
+
+            const response = await request(app)
+                .post(`/videos/${video._id}/delete`)
+                .send();
+
+            const videos = await Video.find({});
+
+            assert.equal(videos.length, 0);
         });
     });
 });
